9a0e4ad56ef38e2f987a32dde0fb26c3e77524e6
romanvbabenko <romanvbabenko@gmail.com>
2011-03-11 08:24:45 +0200

    [^] Test engine

---
+++ spec/loops/engine_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Loops::Engine do
+  # You might have expected an instance of Array
+  specify do
+    # @engine = Loops::Engine.new
+    # @engine.start_loops!(nil).should
+    pending "should raise human error message when loops array is nil"
+  end
+end
